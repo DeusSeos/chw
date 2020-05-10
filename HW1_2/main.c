511be87e6f5dc4451758797c441f4e79f7f32fba
Seos <dchua200@gmail.com>
2020-05-09 23:19:30 -0700

    real working launch, tasks

--- HW1_2/main.c
+++ HW1_2/main.c
@@ -2,21 +2,25 @@
 #include <stdlib.h>
 #define smile "smile!"
 
-int main()
-{
-    printf("%s%s%s\n", smile, smile, smile);
-    printf("%s%s\n", smile, smile);
-    printf("%s", smile);
-    return 0;
-}
 
+void two(){
+    printf("two");
+}
 void one_three(){
     printf("one");
     two() ;
     printf("three");
 
 }
-
-void two(){
-    printf("two");
+int main()
+{
+    int x = 3;
+    x++;
+    x+= 4;
+    printf("%s%s%s\n", smile, smile, smile);
+    printf("%s%s\n", smile, smile);
+    printf("%s", smile);
+    return 0;
 }
+
+
